3151e385a5198e3a702f82a90b75f2671cd88c99
RENaa_FD <92320175+lRENyaaa@users.noreply.github.com>
2025-11-06 19:23:29 +0800

    A tweak

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,38 +1,25 @@
 package sample;
 
 import java.util.Scanner;
+import java.util.function.Function;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Filter4j 演示程序 已经启动!");
         while (true) {
-            String str = scanner.nextLine();
-            doV2(str);
-            doV1(str);
-            doV0(str);
+            String message = scanner.nextLine();
+            filter(filter.v2.TextFilter::isIllegal, message, "V2");
+            filter(filter.v1.TextFilter::isIllegal, message, "V1");
+            filter(filter.v0.TextFilter::isIllegal, message, "V0");
         }
     }
 
-    private static void doV2(String str) {
+    private static void filter(Function<String,Boolean> checker, String message, String version) {
         long l1 = System.nanoTime();
-        System.out.println("V2: " + (filter.v2.TextFilter.isIllegal(str) ? "异常" : "正常"));
+        Boolean result = checker.apply(message);
         long l2 = System.nanoTime();
-        System.out.println("V2过滤耗时: " + (l2 - l1) / 1000_000.0 + " ms");
-    }
-
-    private static void doV1(String str) {
-        long l1 = System.nanoTime();
-        System.out.println("V1: " + (filter.v1.TextFilter.isIllegal(str) ? "异常" : "正常"));
-        long l2 = System.nanoTime();
-        System.out.println("V1过滤耗时: " + (l2 - l1) / 1000_000.0 + " ms");
-    }
-
-
-    private static void doV0(String str) {
-        long l1 = System.nanoTime();
-        System.out.println("V0: " + (filter.v0.TextFilter.isIllegal(str) ? "异常" : "正常"));
-        long l2 = System.nanoTime();
-        System.out.println("V0过滤耗时: " + (l2 - l1) / 1000_000.0 + " ms");
+        System.out.println(version + ": " + (result ? "异常" : "正常"));
+        System.out.println(version + "过滤耗时: " + (l2 - l1) / 1000_000.0 + " ms");
     }
 }
